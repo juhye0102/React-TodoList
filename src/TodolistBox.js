17f782b8dbafe5e3d78736d150c93ba31e0ae023
ohjuhye <bbangbbang562@gmail.com>
2022-06-07 22:03:38 +0900

    TodolistBox.js 추가

---
+++ src/TodolistBox.js
@@ -0,0 +1,14 @@
+// import React from "react";
+import styled from "styled-components";
+
+const TodolistBox = styled.div`
+  width: 500px;
+  height: 750px;
+
+  position: relative;
+  background-color: whitesmoke;
+
+  border-radius: 16px;
+`;
+
+export default TodolistBox;
