01d5467a1bfd463a06e8e82e68d7e1d9825eaec4
ohjuhye <bbangbbang562@gmail.com>
2022-06-09 22:09:33 +0900

    [ 🔨 ] ListInsert.js 수정

--- src/components/Insert/ListInsert.js
+++ src/components/Insert/ListInsert.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 function ListInsert() {
   return (
-    <Container>
+    <div>
       <form>
         <TextInput type="text" name="text" placeholder="이 곳에 작성하세요." />
         <AddButton type="submit">추가</AddButton>
       </form>
-    </Container>
+    </div>
   );
 }
