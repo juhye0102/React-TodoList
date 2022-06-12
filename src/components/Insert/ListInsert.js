40d3f6eb93b317be4f6142279d91efcea53302f4
ohjuhye <bbangbbang562@gmail.com>
2022-06-12 22:28:39 +0900

    [ 🔨 ] ListInsert.js 수정

--- src/components/Insert/ListInsert.js
+++ src/components/Insert/ListInsert.js
@@ -5,9 +5,13 @@ function ListInsert() {
   return (
     <div>
       <form>
-        <TextInput type="text" name="text" placeholder="이 곳에 작성하세요." />
-        <AddButton type="submit">추가</AddButton>
+        <h1 type="text" name="text">
+          할 일을 작성하세요.
+        </h1>
+        <input type="submit">추가</input>
       </form>
     </div>
   );
 }
+
+export default ListInsert;
