a57be3092abe22fa40b12c75fd90a28cfaa9833b
ohjuhye <bbangbbang562@gmail.com>
2022-07-06 22:29:53 +0900

    [ 🔨 ] Item.js 수정

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,23 +2,5 @@ import React from "react";
 import styled from "styled-components";
 
 function Item() {
-  return (
-    <Container>
-      <DoneButton>
-        {checked ? <CheckBox src="./img/check.png" /> : null}
-      </DoneButton>
-      <Topic
-        style={{
-          textDecoration: checked ? "line-through" : null,
-          color: checked ? "#ccc" : "#000",
-        }}
-      >
-        {" "}
-        {text}{" "}
-      </Topic>
-      <DeleteButton>
-        <DeleteImg src="./img/trash.png" />
-      </DeleteButton>
-    </Container>
-  );
+  return <Container></Container>;
 }
